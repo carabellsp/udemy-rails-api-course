b1b6ebf4ba9357f0753baa9276b336724043d0ad
Cara Bell <cara.bell@192.168.1.106>
2021-07-14 12:28:22 +0100

    article model tests WITHOUT using factory

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  subject { described_class.new(title: 'hi', content: 'hello', slug: 'test-slug') }
-  it 'checks for title type' do
-    expect(subject.title).to be_a(String)
+  it 'tests article object' do
+    article = Article.create({title: 'Sample article title', content: 'Sample content', slug: 'article-slug'})
+    # normally we would pass the data as above to create a valid object,
+    # however because we need many of these objects across our test suite, it makes sense to do it via a factory
+    # This also means we don't need to keep storing the logic of creating our article object
   end
 end
