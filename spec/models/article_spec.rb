3492c85b25bf52fe526a4e227322af1320eaca03
Cara Bell <cara.bell@192.168.1.106>
2021-07-14 17:03:50 +0100

    build validation specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -30,5 +30,25 @@ RSpec.describe Article, type: :model do
     it 'tests article is valid' do
       expect(article).to be_valid # same as article.valid? == true
     end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'has invalid content' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'validates the uniqueness of the slug' do
+      article1 = create(:article)
+      expect(article1).to be_valid
+      article2 = build(:article, slug: article1.slug)
+      expect(article2).not_to be_valid
+      expect(article2.errors[:slug]).to include('has already been taken')
+    end
   end
 end
