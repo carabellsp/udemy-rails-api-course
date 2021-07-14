60e8615a01e8c400d65fa1ba927f54d227bb40ca
Cara Bell <cara.bell@192.168.1.106>
2021-07-14 12:31:45 +0100

    refactor article model test to use factorybot

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    title { "Sample article title" }
+    content { "Sample content" }
+    slug { "sample-slug" }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
+# RSpec.describe Article, type: :model do
+#   it 'tests article object' do
+#     article = Article.create({title: 'Sample article title', content: 'Sample content', slug: 'article-slug'})
+#   end
+# end
+
+# normally we would pass the data as above to create a valid object,
+# however because we need many of these objects across our test suite, it makes sense to do it via a factory
+# This also means we don't need to keep storing the logic of creating our article object
+
 RSpec.describe Article, type: :model do
   it 'tests article object' do
-    article = Article.create({title: 'Sample article title', content: 'Sample content', slug: 'article-slug'})
-    # normally we would pass the data as above to create a valid object,
-    # however because we need many of these objects across our test suite, it makes sense to do it via a factory
-    # This also means we don't need to keep storing the logic of creating our article object
+    article = FactoryBot.create(:article) # :article is the factory name
+    expect(article.title).to eq('Sample article title')
+    expect(article.title).to be_a(String)
   end
 end
